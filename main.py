d8a3c10068422e1190782d143aa7d2aef05cfeeb
Andre <apcianflone@gmail.com>
2017-08-31 12:11:35 -0400

    refac

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import codecs
 import json
 import pickle
 import numpy as np
-from sentiment_neuron.utils import sst_binary
-from sentiment_neuron.utils import train_with_reg_cv as log_regression_sk
-from models import log_regression_tf
+from models import log_regression_tf, log_regression_sk
+from helper import histogram, bar_chart, get_x_y
+import matplotlib.pyplot as plt
 import time
 
 # Load pickled data
@@ -14,49 +14,53 @@ data = pickle.load(open("neurons.pickle", "rb"))
 ids, h_arr = data['ids'], data['discs']
 h_size = h_arr.shape[1]
 
-def get_x_y(path, relation=None):
-  map_cls = {'positive': 1, 'negative': 0}
-  x_size = sum(1 for line in open(path))
-  x = []
-  y = []
-  row = 0
-  with codecs.open(path, encoding='utf8') as f:
-    for line in f:
-      j = json.loads(line)
-
-      # Check if wanted relation
-      if j['Relation'] != relation: continue
-
-      dID  = j['ID']
-      y_sample = map_cls[j['Class']]
-
-      # Get the row of the hidden units value
-      x_sample = h_arr[ids[dID]]
-      x.append(x_sample)
-      y.append(y_sample)
-  x = np.array(x)
-  y = np.array(y)
-  return x, y
-
-relation = 'Contingency'
-trX, trY =  get_x_y('data/one_v_all_train.json', relation)
-vaX, vaY =  get_x_y('data/one_v_all_dev.json', relation)
-teX, teY =  get_x_y('data/one_v_all_test.json', relation)
 
 # Tensorflow log reg
-print('Training with tensorflow logistic regression')
-t1 = time.time()
-f1_test, f1_val, coefs = log_regression_tf(trX, trY, vaX, vaY, teX, teY)
-print('training min: {}'.format(round((time.time() - t1)/60, 1)))
-print('%05.2f test f1'%f1_test)
-print('%05.2f validation f1'%f1_val)
+# print('Training with tensorflow logistic regression')
+# t1 = time.time()
+# f1_test, f1_val, coefs = log_regression_tf(trX, trY, vaX, vaY, teX, teY)
+# print('training min: {}'.format(round((time.time() - t1)/60, 1)))
+# print('%05.2f test f1'%f1_test)
+# print('%05.2f validation f1'%f1_val)
 
-# SKlearn log reg
-print('Training with sklearn logistic regression')
-t1 = time.time()
-full_rep_acc, c, nnotzero = log_regression_sk(trX, trY, vaX, vaY, teX, teY)
-print('training min: {}'.format(round((time.time() - t1)/60, 1)))
-print('%05.2f test accuracy'%full_rep_acc)
-print('%05.2f regularization coef'%c)
-print('%05d features used'%nnotzero)
+# Which relation?
+relations = ['Temporal', 'Contingency', 'Expansion', 'Comparison']
+folders = ['binary_implicit', 'binary_all_rel', 'binary_explicit']
 
+def train(folder, relation):
+  pass
+
+plot_number = 1
+for relation in relations:
+  print('**Testing on relation: ', relation)
+  for folder in folders:
+    train_path = 'data/'+folder+'/one_v_all_train.json'
+    val_path = 'data/'+folder+'/one_v_all_dev.json'
+    test_path = 'data/'+folder+'/one_v_all_test.json'
+    # Get data
+    trX, trY =  get_x_y(ids, h_arr, train_path, relation)
+    vaX, vaY =  get_x_y(ids, h_arr, val_path, relation)
+    teX, teY =  get_x_y(ids, h_arr, test_path, relation)
+
+    # SKlearn log reg
+    print('Training with sklearn logistic regression')
+    t1 = time.time()
+    full_rep_acc, c, nnotzero, notzero_coefs_ids, notzero_coefs = \
+                                log_regression_sk(trX, trY, vaX, vaY, teX, teY)
+    print('training min: {}'.format(round((time.time() - t1)/60, 1)))
+    print('%05.2f test accuracy'%full_rep_acc)
+    print('%05.2f regularization coef'%c)
+    print('%05d features used'%nnotzero)
+
+    # Chart
+    title = relation + ' ' + folder
+    label = 'test acc {0:0.2f}, reg coef {1:0.2f}, features used: {2:0.0f}'.format(\
+                                                        full_rep_acc, c, nnotzero)
+    bar_chart(notzero_coefs, title, label, 4,3,plot_number)
+    plot_number += 1
+plt.show()
+
+
+# bar_chart(notzero_coefs)
+# histogram(notzero_coefs, bins=100)
+print('goodbye world')
